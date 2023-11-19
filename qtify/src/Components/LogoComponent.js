5efc15f8281fc1a0a078d92413e666cb61f5b811
Sanyam Singla <singlasanyam214@gmail.com>
2023-11-19 13:23:11 +0530

    bug fixes

--- qtify/src/Components/LogoComponent.js
+++ qtify/src/Components/LogoComponent.js
@@ -1,6 +1,8 @@
 import navBarContainerStyles from "../css/NavBarComponent.module.css"
 export default function LogoComponent(){
     return (
-        <img className={navBarContainerStyles.logoCss} src={require('../assets/qtifyLogo.png')} />
+        <div className={navBarContainerStyles.logoWrapper}>
+            <img className={navBarContainerStyles.logoCss} src={require('../assets/qtifyLogo.png')} />
+        </div>
     )
 }
