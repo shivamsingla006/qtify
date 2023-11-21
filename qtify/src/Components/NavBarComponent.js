ca373d40c7cc7952cc1fb36ef326a8de5de5a337
Sanyam Singla <singlasanyam214@gmail.com>
2023-11-21 12:30:08 +0530

    included nav bar as wrapper

--- qtify/src/Components/NavBarComponent.js
+++ qtify/src/Components/NavBarComponent.js
@@ -5,16 +5,10 @@ import searchStyle from "../css/SearchInputField.module.css"
 import LogoComponent from "./LogoComponent.js"
 export default function NavBarComponent(){
     return (
-        <div className={navBarContainerStyles.navbarcontainer}>
-           {/* <LogoComponent /> */}
-           <div className={navBarContainerStyles.logoWrapper}>
-                <img className={navBarContainerStyles.logoCss} src={require('../assets/qtifyLogo.png')} />
-            </div>
-            <div className={searchStyle.searchWrapper}>
-                <input className={searchStyle.inputStyle} type="text" placeholder="Search a album of your choice" />
-                <ButtonComponent containIcon={true} buttonStyle={searchStyle.buttonStyle} />
-            </div>
+        <nav className={navBarContainerStyles.navbarcontainer}>
+           <LogoComponent />
+           <SearchInputField />
            <ButtonComponent buttonStyle={searchStyle.giveFeedbackBtn} containIcon={false} text={'Give Feedback'} />
-        </div>
+        </nav>
     )
 }
