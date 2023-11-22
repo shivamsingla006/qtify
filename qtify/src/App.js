573d0c2362ad1dd3f8193622cf07a43e343e9040
Sanyam Singla <singlasanyam214@gmail.com>
2023-11-22 16:21:24 +0530

    recent updates

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,21 @@
-import NavBarComponent from "./Components/NavBarComponent.js";
+// import NavBarComponent from "./Components/NavBarComponent.js";
+import navBarContainerStyles from "./css/NavBarComponent.module.css"
+import SearchInputField from "./Components/SearchInputField.js"
+import ButtonComponent from "./Components/ButtonComponent.js"
+import searchStyle from "./css/SearchInputField.module.css"
+import LogoComponent from "./Components/LogoComponent.js"
 
 function App() {
   return (
-    <div className="App">
-        <NavBarComponent />
+    <div className={navBarContainerStyles.navbarcontainer}>
+        {/* <NavBarComponent /> */}
+        {/* <nav className={navBarContainerStyles.navbarcontainer}> */}
+           <LogoComponent />
+           <SearchInputField />
+           <div>
+           <ButtonComponent buttonStyle={searchStyle.giveFeedbackBtn} containIcon={false} text={'Give Feedback'} />
+           </div>
+        {/* </nav> */}
     </div>
   );
 }
